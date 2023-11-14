5a3d242a587ecd2b46dc111da0b77ab54f913e16
MirMosarofHossain <mirmosarofhossain3010@gmail.com>
2023-11-14 12:10:18 +0530

    separate all file

---
+++ script.js
@@ -0,0 +1,21 @@
+let cel=document.getElementById("cel");
+    let farh= document.getElementById("farh");
+
+    cel.addEventListener('input',function(){
+        let c=this.value;
+        let f = (c*9/5)+32;
+        if(!Number.isInteger(f)){
+            f=f.toFixed(4);
+        }
+        farh.value=f;
+    });
+
+    farh.addEventListener('input',function(){
+        let f=this.value;
+      
+        let c=(f-32)*5/9;
+        if(!Number.isInteger(c)){
+           c= c.toFixed(4);
+        }
+        cel.value=c;
+    });
